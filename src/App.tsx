42101490e4786377cf6c2413649ecef00c094ebf
koinapu <koi.inapu@gmail.com>
2021-05-05 22:43:40 +0900

    localで動くよう調整

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import "./styles.css";
 export default function App() {
   return (
     <ChakraProvider>
-      <Button colorScheme="teal">ボタン</Button>
+      <Button colorScheme="blue">ボタン</Button>
     </ChakraProvider>
   );
 }
