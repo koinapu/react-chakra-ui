2ea30a0a4791f648dad90784a63ca0946fface50
koinapu <koi.inapu@gmail.com>
2021-05-05 23:45:58 +0900

    router追加

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,13 @@
-import {extendTheme} from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
-styles: {
-    global: {
-        body: {
-            backgroundColor: "gray.300",
-            color: "gray.800"
+    styles: {
+        global: {
+            body: {
+                backgroundColor: "gray.300",
+                color: "gray.800"
+            }
         }
     }
-}
 });
 export default theme;
